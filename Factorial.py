74d9904bea7626fa0fb2ba146d2c6222182f0b01
Harshad Prajapati <88772588+iharshadprajapati@users.noreply.github.com>
2021-09-12 13:50:43 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,29 @@
+# n! = n * n-1 * n-2 * n-3.......1
+# n! = n * (n-1)!
+def factorial_iterative(n):
+    """
+        :param n: Integer
+        :return: n * n-1 * n-2 * n-3.......1
+    """
+    fac = 1
+    for i in range(n):
+        fac = fac * (i+1)
+    return fac
+
+def factorial_recursive(n):
+    """
+        :param n: Integer
+        :return: n * n-1 * n-2 * n-3.......1
+    """
+    if n ==1:
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
+    # 5 * factorial_recursive(4)
+    # 5 * 4 * factorial_recursive(3)
+    # 5 * 4 * 3 * factorial_recursive(2)
+    # 5 * 4 * 3 * 2 * factorial_recursive(1)
+    # 5 * 4 * 3 * 2 * 1 = 120
+number = int(input("Enter then number"))
+print("Factorial Using Iterative Method", factorial_iterative(number))
+print("Factorial Using Recursive Method", factorial_recursive(number))
